05c3bfbeb8f6352b81865e9dec70120af9849dea
AaronB <aaronbesson@gmail.com>
2019-10-02 15:23:18 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,94 +1,89 @@
-import React, {useState, useCallback} from 'react';
-import {useDropzone} from 'react-dropzone'
+import React, { useState, useCallback } from 'react';
+import { useDropzone } from 'react-dropzone'
 
 
 function App() {
-  const [name,setName] = useState('');
-  const [status,setStatus] = useState('');
-  const [email,setEmail] = useState('');
-  const [message,setMessage] = useState('');
-  const [file, setFile] = useState({});
-  
-   const onDrop = useCallback(acceptedFiles => {
-    console.log(acceptedFiles)
-    setFile(acceptedFiles[0])
-  }, [])
-  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+    const [name, setName] = useState('');
+    const [status, setStatus] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    const [file, setFile] = useState({});
 
-  const encode = (data) => {
-    const formData = new FormData();
-    Object.keys(data).forEach((k)=>{
-      formData.append(k,data[k])
-    });
-    return formData
-  }
+    const onDrop = useCallback(acceptedFiles => {
+        console.log(acceptedFiles)
+        setFile(acceptedFiles[0])
+    }, [])
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
 
-  const handleSubmit = e => {
-    const data = { "form-name": "contact", name, email, message, file }
-    
-    fetch("/", {
-      method: "POST",
-      // headers: { "Content-Type": 'multipart/form-data; boundary=random' },
-      body: encode(data)
-    })
-      .then(() => setStatus("Form Submission Successful!!"))
-      .catch(error => setStatus("Form Submission Failed!"));
-
-    e.preventDefault();
-  };
-
-  const handleChange = e => {
-    const {name, value} = e.target
-    if (name === 'name' ){
-      return setName(value)
+    const encode = (data) => {
+        const formData = new FormData();
+        Object.keys(data).forEach((k) => {
+            formData.append(k, data[k])
+        });
+        return formData
     }
-    if (name === 'email' ){
-      return setEmail(value)
-    }
-    if (name === 'message' ){
-      return setMessage(value)
-    }
-  }
 
-  return (
-    <div className="App">
-    <form onSubmit={handleSubmit} action="/thank-you/">
-          <p>
-            <label>
-              Your Name: <input type="text" name="name" value={name} onChange={handleChange} />
-            </label>
-          </p>
-          <p>
-            <label>
-              Your Email: <input type="email" name="email" value={email} onChange={handleChange} />
-            </label>
-          </p>
-          <p>
-            <label>
-              Message: <textarea name="message" value={message} onChange={handleChange} />
-            </label>
-          </p>
-          <div {...getRootProps()}>
-            <label>
-              Drivers Permit: <input type="file" data-secure-field name="fileToUpload_permit" id="fileToUpload_permit" />
+    const handleSubmit = e => {
+        const data = { "form-name": "contact", name, email, message, file }
+
+        fetch("/", {
+            method: "POST",
+            // headers: { "Content-Type": 'multipart/form-data; boundary=random' },
+            body: encode(data)
+        })
+            .then(() => setStatus("Form Submission Successful!!"))
+            .catch(error => setStatus("Form Submission Failed!"));
+
+        e.preventDefault();
+    };
+
+    const handleChange = e => {
+        const { name, value } = e.target
+        if (name === 'name') {
+            return setName(value)
+        }
+        if (name === 'email') {
+            return setEmail(value)
+        }
+        if (name === 'message') {
+            return setMessage(value)
+        }
+    }
+
+    return (
+        <div className="App">
+            <form onSubmit={handleSubmit} action="/thank-you/">
+                <p>
+                    <label>
+                        Your Name: <input type="text" name="name" value={name} onChange={handleChange} />
+                    </label>
+                </p>
+                <p>
+                    <label>
+                        Your Email: <input type="email" name="email" value={email} onChange={handleChange} />
+                    </label>
+                </p>
+                <p>
+                    <label>
+                        Message: <textarea name="message" value={message} onChange={handleChange} />
+                    </label>
+                </p>
+                <div {...getRootProps()}>
+                    <label>Drivers Permit: </label>
+                    <input type="file" data-secure-field name="fileToUpload_permit" id="fileToUpload_permit" /></label>
                 {
-              isDragActive ?
-                <p>Drop the files here ...</p> :
-                <p>Drag 'n' drop some files here, or click to select files</p>
-            }
-            </label>
+                    isDragActive ?
+                        <p>Drop the files here ...</p> :
+                        <p>Drag 'n' drop some files here, or click to select files</p>
+                }
+            
           </div>
-          <p>
-            <button type="submit">Send</button>
-          </p>
+            <p>
+                <button type="submit">Send</button>
+            </p>
         </form>
         <h3>{status}</h3>
-    </div>
+    </div >
   );
 }
-
-<label>Drivers Permit: <input type="file" data-secure-field name="fileToUpload_permit" id="fileToUpload_permit" type="file" /></label>
-      <label>Passport Permit: <input type="file" data-secure-field name="fileToUpload_pass" id="fileToUpload_pass" type="file" /></label>
-      <label>Utility Bill: <input type="file" data-secure-field name="fileToUpload_bill" id="fileToUpload_bill" type="file" /></label>
-
 export default App;
