74268beb915ca5fa7d7a87053a1153e3796680da
AaronB <aaronbesson@gmail.com>
2019-10-02 15:04:18 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -40,7 +40,7 @@ export default class Index extends React.Component {
               <form
                 name="contact"
                 method="post"
-                action="https://ttutc.com"
+                action="index.js"
                 data-netlify="true"
                 data-netlify-honeypot="bot-field"
                 onSubmit={this.handleSubmit}
